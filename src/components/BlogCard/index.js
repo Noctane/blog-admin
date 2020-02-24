f53da369ca984b0b4219f54b418649b3b8c1d994
Arnaud Prat <arnaud.prat@gmail.com>
2020-02-24 21:29:12 +0100

    ADD some style to blog list

--- src/components/BlogCard/index.js
+++ src/components/BlogCard/index.js
@@ -6,10 +6,15 @@ function BlogCard({ blog, handleAction }) {
   const { url } = useRouteMatch();
 
   return (
-    <div className="p-4 border border-gray-200">
-      <h2>{blog.name}</h2>
-      <Link to={`${url}/${blog.id}`}>Voir le détail</Link>
-      <button onClick={() => handleAction(blog.id)}>delete</button>
+    <div className="p-4 border border-gray-200 rounded-md">
+      <div className="mb-4">
+        <h2 className="text-lg font-medium">{blog.name}</h2>
+        <em className="text-gray-600">{`${blog.articles.length} articles`}</em>
+      </div>
+      <div className="flex justify-between">
+        <Link className="bg-blue-500 py-2 px-4 rounded-md text-white" to={`${url}/${blog.id}`}>Gérer</Link>
+        <button className="bg-red-500 py-2 px-4 rounded-md text-white" onClick={() => handleAction(blog.id)}>delete</button>
+      </div>
     </div>
   );
 }
--- src/container/BlogList/index.js
+++ src/container/BlogList/index.js
@@ -45,19 +45,23 @@ console.log('blogs', url);
 
   return (
     <div className="container mx-auto">
-      <h1 className="text-xl font-semibold mb-4">Mes blogs</h1>
-      <div className="grid gap-2 grid-cols-3">
-        {list.length > 0 &&
-          list.map(blog => (
-            <BlogCard blog={blog} key={blog.id} handleAction={onClickDelete} />
-          ))
-        }
-      </div>
-
-      <form>
-        <input type="text" onChange={onInputChange} placeholder="title" />
-        <button type="submit" onClick={onClickSubmitBtn}>submit</button>
-      </form>
+      <section className="mb-6">
+        <h1 className="text-2xl font-semibold mb-4">Mes blogs</h1>
+        <div className="grid gap-2 grid-cols-3">
+          {list.length > 0 &&
+            list.map(blog => (
+              <BlogCard blog={blog} key={blog.id} handleAction={onClickDelete} />
+              ))
+            }
+        </div>
+      </section>
+      <section>
+        <h2 className="text-xl font-semibold mb-4">Ajouter un blog</h2>
+        <form>
+          <input className="p-4 border border-gray-200 rounded-md" type="text" onChange={onInputChange} placeholder="title" />
+          <button type="submit" onClick={onClickSubmitBtn}>submit</button>
+        </form>
+      </section>
     </div>
   )
 }
