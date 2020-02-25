7428396867a1b49134d2922c637a5906c04037fc
Arnaud Prat <arnaud.prat@gmail.com>
2020-02-25 20:30:29 +0100

    ADD logout promise

--- src/container/App/AuthContext.js
+++ src/container/App/AuthContext.js
@@ -1,12 +1,12 @@
 import React, { useState, createContext } from 'react';
-import { login } from '../../helpers/auth';
+import { login, logout } from '../../helpers/auth';
 
 export const AuthContext = createContext({});
 
 export default function Auth({ children }) {
   const [isAuth, setIsAuth] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-
+console.log('localStorage', localStorage);
   const authLogin = credentials => {
     setIsLoading(true)
     login(credentials)
@@ -24,10 +24,12 @@ export default function Auth({ children }) {
 
   const authLogout = () => {
     setIsLoading(true)
-    setTimeout(() => {
-      setIsAuth(false)
-      setIsLoading(false)
-    }, 2000)
+    logout()
+      .then(setTimeout(() => {
+        setIsAuth(false)
+        setIsLoading(false)
+      }, 2000)
+    );
   }
 
   return (
--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,9 +1,15 @@
 export function login(credentials) {
   return new Promise((resolve, reject) => {
     if (credentials.login === 'overblog' && credentials.password === 'overblog') {
-      resolve('connecté');
+      resolve(localStorage.setItem('token', Date.now()));
     } else {
       reject('wrong creds');
     }
+  });
+}
+
+export function logout() {
+  return new Promise((resolve, reject) => {
+    resolve(localStorage.clear())
   });
 }
