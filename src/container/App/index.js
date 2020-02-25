4a99651f3a53260441d535225b29f71753a183a7
Arnaud Prat <arnaud.prat@gmail.com>
2020-02-25 13:52:37 +0100

    Clean code

--- src/container/App/index.js
+++ src/container/App/index.js
@@ -1,9 +1,8 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import Auth from './AuthContext';
 import PrivateRoute from './Acl/PrivateRoute';
